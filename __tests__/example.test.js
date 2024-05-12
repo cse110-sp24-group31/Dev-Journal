952ba60eedacdae2a2880c679989520c2f95031c
NikhilMahendran <150480880+NikhilMahendran@users.noreply.github.com>
2024-05-12 16:21:28 -0700

    Update example.test.js

--- __tests__/example.test.js
+++ __tests__/example.test.js
@@ -3,3 +3,6 @@ import { addNumbers } from '../testcode/example';
 test('adds 1 + .. 10 to equal 55', () => {
     expect(addNumbers(10)).toBe(55);
 });
+test('adds 1 + .. 10 to equal 55', () => {
+    expect(1+2+3+4+5+6+7+8+9+10).toBe(55);
+});
