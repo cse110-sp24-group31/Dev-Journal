97762f1e7c548baad08eaddd0a4ff9c72b528e18
NikhilMahendran <150480880+NikhilMahendran@users.noreply.github.com>
2024-05-12 16:06:53 -0700

    Update example.test.js

--- __tests__/example.test.js
+++ __tests__/example.test.js
@@ -1,11 +1,5 @@
-const addNumbers = require('./testcode/example.js'); 
+import { addNumbers } from '../testcode/example';
 
-describe('addNumbers', () => {
-    test('adds numbers from 1 to n', () => {
-        let sum = 0;
-        for (let n = 1; n <= 10; n++) {
-            sum += n;
-            expect(addNumbers(n)).toBe(sum);
-        }
-    });
+test('adds 1 + .. 10 to equal 55', () => {
+    expect(10).toBe(55);
 });
