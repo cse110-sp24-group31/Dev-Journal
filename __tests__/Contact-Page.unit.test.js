d35d271e6cb1d5592e40aa0ef3079871d96f7c49
azuk132435 <ket016@ucsd.edu>
2024-06-03 12:49:24 -0700

    changes

--- __tests__/Contact-Page.unit.test.js
+++ __tests__/Contact-Page.unit.test.js
@@ -11,7 +11,7 @@ describe('Unit test: contact management', () => {
     });
     page = await browser.newPage();
     await page.setViewport({ width: 1920, height: 1080 });
-    await page.goto('http://127.0.0.1:5501/');
+    await page.goto('https://cse110-sp24-group31.github.io/Dev-Journal/');
     // Click the "Contacts" link
     await page.click('.nav-links a:nth-child(3)');
   });
