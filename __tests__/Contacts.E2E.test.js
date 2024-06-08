ddd82fddcb498388984adf5b4970dad728cc3e70
bwhenn <brwen@ucsd.edu>
2024-06-08 02:02:22 -0700

    tests

--- __tests__/Contacts.E2E.test.js
+++ __tests__/Contacts.E2E.test.js
@@ -1,26 +1,29 @@
 const puppeteer = require('puppeteer');
 
-describe('Unit test: contact management', () => {
-  let browser;
-  let page;
 
-  beforeAll(async () => {
-    browser = await puppeteer.launch({
-      headless: false,
-      args: ['--start-maximized']
+  describe('E2E test: contact management', () => {
+    beforeAll(async () => {
+      await page.goto('https://cse110-sp24-group31.github.io/Dev-Journal/'); //change this for live server
     });
-    page = await browser.newPage();
-    await page.setViewport({ width: 1920, height: 1080 });
-    await page.goto('https://cse110-sp24-group31.github.io/Dev-Journal/');
-    // Click the "Contacts" link
-    await page.click('.nav-links a:nth-child(1)');
+    it('should have no project in the local storage', async () => {
+      const res = await page.evaluate(() => {
+        localStorage.getItem('projects');
+      });
+      expect(res).toBe(undefined);
+    });
+  
+    let addProjectCardBtnHandle;
+    it('should add contact', async () => {
+      addProjectCardBtnHandle = await page.$('.add-project-card');
+      expect(addProjectCardBtnHandle).not.toBe(null);
+    });
+  
+    let addProjectCardModalHandle;
+    it('should delete contact using id', async () => {
+      addProjectCardModalHandle = await page.$('#addCardModal');
+      const isHidden = await page.$eval('#addCardModal', modal => {
+        return window.getComputedStyle(modal).display;
+      });
+      expect(isHidden).toBe('none');
+    }); 
   });
-
-
-  it('should create and delete a contact', async () => {
-    await page.type('input[name="name"]', 'Jane Doe');
-    await page.click('form input[type="submit"]');
-    
-    // Verify the new contact is added
-  });
-});
